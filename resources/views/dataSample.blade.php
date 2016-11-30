a6a83fad0a67d6cba2767a2dcbc589fc981f877e
Schaffora <schaffora@gmail.com>
2016-11-30 15:54:01 +0100

    Data import part DONE !

--- resources/views/dataSample.blade.php
+++ resources/views/dataSample.blade.php
@@ -1,23 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Data add</div>
-
-                <div class="panel-body">
-                {!! Form::open(['route' => 'addDataSample']) !!}
-				<div class="form-group">
-					{!! Form::label('File to import') !!}
-					{!! Form::text('dataSample', null, array('required','class'=>'form-control', 'placeholder'=>'File name')) !!}
-					{!! Form::submit('Submit!', ['class' => 'btn btn-info pull-right']) !!}
-				</div>
-				{!! Form::close() !!}
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Import personnal Stayfit data</div>
+                    <div class="panel-body">
+                        {!! Form::open(['route' => 'addDataSample']) !!}
+                        <div class="form-group">
+                            {!! Form::label('File to import') !!}
+                            {!! Form::file('dataSample', null, array('required','class'=>'form-control', 'placeholder'=>'File name')) !!}
+                            {!! Form::submit('Submit!', ['class' => 'btn btn-info pull-right']) !!}
+                        </div>
+                        {!! Form::close() !!}
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>	
 @endsection
--- resources/views/dataSampleOk.blade.php
+++ resources/views/dataSampleOk.blade.php
@@ -1,24 +1,43 @@
 @extends('layouts.app')
 
 @section('content')
-	<div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Data add</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Imported data content</div>
 
-                <div class="panel-body">
-				{{$message}}
+                    <div class="panel-body">
 
-                <h2>Points</h2>
-                <ul>
-                @foreach($session->geometry as $point)
-                    <li>{{ $point[0] }}, {{ $point[1] }}</li>
-                @endforeach
-                </ul>
+                        <p><b>Required time</b></p>
+                        <ul>
+                            <li>{{$session->duration}}</li>
+                        </ul>
+
+                        <p><b>Covered distance</b></p>
+                        <ul>
+                            <li>{{$session->distance}}</li>
+                        </ul>
+
+                        <p><b>Burned calories</b></p>
+                        <ul>
+                            <li>{{$session->calories}}</li>
+                        </ul>
+
+                        <p><b>Needed footsteps</b></p>
+                        <ul>
+                            <li>{{$session->footsteps}}</li>
+                        </ul>
+
+                        <p><b>Visited Geopositions</b></p>
+                        <ul>
+                            @foreach($session->geometry as $point)
+                                <li>{{ $point[0] }}, {{ $point[1] }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-	</div>
 @endsection
