ec9be7e7ee2897ff8f0474ea41039415ec961d5b
lancelot <lancelotlancelot94@gmail.com>
2016-12-05 14:04:09 +0100

    begining of dataSelection

--- resources/views/dataSelection.blade.php
+++ resources/views/dataSelection.blade.php
@@ -1,17 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Dashboard</div>
 
-                <div class="panel-body">
-                    dataselection
+                    <div class="panel-body">
+                        dataselection
+
+                        @section('data')
+                            {{!! $currentId=Auth::user()->id !!}}
+                            {{!! $session=App\Session::where('user_id',3)->first() !!}}
+                        @endsection
+                        {{--{{$session}}--}}
+
+                        <ul class="list-group">
+                            <li class="list-group-item">
+                                Day {{$session->date}}
+                                Duration {{$session->duration }}
+                                Distance {{$session->distance }}
+                                Calories {{$session->calories }}
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
