09ffe02584240556f9a470ec7daf54064532900f
Schaffora <schaffora@gmail.com>
2016-12-05 11:04:10 +0100

    Laravel Version 5.2 -> 5.3

--- config/app.php
+++ config/app.php
@@ -156,8 +156,8 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-		Collective\Html\HtmlServiceProvider::class,
         Khill\Lavacharts\Laravel\LavachartsServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
 
     ],
 
@@ -204,8 +204,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-		'Form'  => Collective\Html\FormFacade::class,
-		'Html'  => Collective\Html\HtmlFacade::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,13 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
+                <div class="panel-heading"></div>
 
-                <div class="panel-body">
-                    <image img src="{{URL::asset('/assets/images/stayfit.png')}}"></image>
+                <div class="panel-body" align="center">
+                    <image img src="{{asset('/assets/images/stayfit.png')}}"></image>
+                    <p><b>Bienvenu dans StayFit</b></p>
+                    <p>Ce projet a été réalisé dans le cadre de l'école d'ingénieur de Neuchâtel en 2016. ©</p>
+                    <p>Il permet la récupération et visualisation de vos efforts physiques du quotidien à l'aide de votre smartphone.</p>
                 </div>
             </div>
         </div>
