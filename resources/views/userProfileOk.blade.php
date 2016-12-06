cd0c967eede54038f58bd3c697dc5d33bfac20ab
Gygi Damien <damien.gygi@he-arc.ch>
2016-12-06 09:15:24 +0100

    Amélioration de la vue userProfile

---
+++ resources/views/userProfileOk.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Data update</div>
+
+                    <div class="panel-body">
+                        Your personal data has been updated successfully
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
